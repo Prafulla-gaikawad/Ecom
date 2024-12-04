4144fd1aa227d878224ffe9df202b922c4cc6a7b
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2024-12-04 23:12:25 +0530

    images imported

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import {
+  Navigate,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./App.css";
+import { Login } from "./features/auth/components/Login";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
