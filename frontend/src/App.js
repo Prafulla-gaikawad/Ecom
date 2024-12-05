5bce2f83f64bead99d084da43bea679ff46a9b78
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2024-12-05 17:06:39 +0530

    theme added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import { useSelector } from "react-redux";
-import {
-  Navigate,
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route} from  'react-router-dom'
+// import {
+//   Navigate,
+//   Route,
+//   RouterProvider,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
 import "./App.css";
 import { Login } from "./features/auth/components/Login";
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
   );
 }
 
